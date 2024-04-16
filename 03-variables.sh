6f6c4de2e5941c8f44df5105d9c9a6fc60b832ca
Divya-madhuri123 <divyamadhuri822@gmail.com>
2024-04-16 18:05:09 +0530

    hello script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
+
 PERSON1=Sachin
 PERSON2=Rahul
 
 echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1: I am fine too. how is your work?"
 echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
