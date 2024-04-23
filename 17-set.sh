fb967ce7bc2bbae3d6771ef4abf58b67eb9b6309
Divya-madhuri123 <divyamadhuri822@gmail.com>
2024-04-23 19:31:52 +0530

    functions

---
+++ 17-set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root access."
+    exit 1
+else
+    echo "you are super user."
+fi
+
+dnf install mystafgsh -y
+dnf install git -y
+
+echo "is script proceeding?"
